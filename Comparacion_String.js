8238ff1703bdb7ab634f930f399f3f1cbe6d0d25
Lukas <lucasbarrientos7@hotmail.com>
2021-02-22 18:12:24 -0300

    Ejemplos de algunos usos de las herramientas de JavaScript

---
+++ Comparacion_String.js
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Comparacion_String {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String nombre1 = "Lucas";
+		String nombre2 = "Lucas";
+
+		if (nombre1.contentEquals(nombre2));
+			System.out.println("Los nombres son iguales");
+	}
+}
---
+++ Ejemplo_CadenaInvertida.js
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Ejemplo_CadenaInvertida {
+    public static String pedirString(){
+    	Scanner sc=new Scanner(System.in);
+    	System.out.println("Ingrese una texto");
+    	return sc.nextLine();
+    }
+
+    public static String invertir(String s){
+    	String nueva="";
+    	for (int i=s.length()-1;i >= 0;i-- ){
+    		nueva=nueva+s.charAt(i);
+    	}
+    	return nueva;
+    }
+
+	public static void main(String[] args) {
+		String cadena=pedirString();
+		String invertida=invertir(cadena);
+		System.out.println("La cadena "+ '"'+cadena+'"'+" invertida es "+'"'+invertida+'"');
+	}
+}
